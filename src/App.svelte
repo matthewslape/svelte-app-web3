600fcc826ec01d18d68f58de4d5df21987db74f4
matthewslape <75864370+matthewslape@users.noreply.github.com>
2021-05-18 14:13:44 -0700

    add for each loop

--- src/App.svelte
+++ src/App.svelte
@@ -13,8 +13,9 @@
 </script>
 
 <h1>Hello {name}!</h1>
+{#each todos as todo, index}
 <input bind:value={todos[index]}>
 <button on:click={removeTodo}>X</button>
-
 <br>
+{/each}
 <button on:click={addTodo}>Add Todo</button>
