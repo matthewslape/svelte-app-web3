697baef99f29d8a60da382b636857741c192288e
matthewslape <75864370+matthewslape@users.noreply.github.com>
2021-05-18 14:22:28 -0700

    push

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
-	let name = 'Matthew';
+	let name;
+	let message;
 	let todos = [];
+
+	function helloThere(name){
+		message = `Hello, ${name}!`;
+	}
 
 	function removeTodo(index){
 		todos = [...todos.slice(0, index), ...todos.slice(0, index + 1)];
@@ -12,7 +17,9 @@
 
 </script>
 
-<h1>Hello {name}!</h1>
+<h3>please enter your name :)</h3>
+<input bind:value={name}>
+<h1 on:blur={helloThere}>{message}</h1>
 {#each todos as todo, index}
 <input bind:value={todos[index]}>
 <button on:click={removeTodo}>X</button>
