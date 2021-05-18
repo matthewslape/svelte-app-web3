c4f91722167ca182eda530168a3a816e63793a4b
matthewslape <75864370+matthewslape@users.noreply.github.com>
2021-05-18 14:05:45 -0700

    Start of todoList

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script>
 	let name = 'Matthew';
+	let todos = [];
+
+	function removeTodo(){
+		todos = [...todos.slice(0, index), ...todos.slice(0, index + 1)];
+	}
+
+	function addTodo(){
+		todos = [...todos, ''];
+	}
+
 </script>
 
 <h1>Hello {name}!</h1>
+<input bind:value={todos[index]}>
+<button on:click={removeTodo}>X</button>
+
+<br>
+<button on:click={addTodo}>Add Todo</button>
