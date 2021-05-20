a4a276c8a0fb3cc6f625bc61aed44467d358416f
matthewslape <75864370+matthewslape@users.noreply.github.com>
2021-05-20 07:49:18 -0700

    centered and styled

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-	let name;
 	let n = "Human?";
 	let todos = [];
 
@@ -14,15 +13,44 @@
 	function addTodo(){
 		todos = [...todos, ''];
 	}
-
 </script>
 
-<h1 on:click={helloThere}>Hello, <span style="color: #063c81">{n}</span></h1>
-<input style="background: black; color: white;"bind:value={n}>
-<br>
-{#each todos as todo, index}
-<input bind:value={todos[index]}>
-<button on:click={()=>removeTodo(index)}>X</button>
-<br>
-{/each}
-<button on:click={addTodo}>Add Something</button>
+<style scoped>
+	body{
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+	.list{
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background: slategray;
+		border-radius: 10px;
+		padding: 10px;
+	}
+	.todo{
+		display: flex;
+		flex-direction: row;
+	}
+
+</style>
+
+<body>
+	<h1 on:click={helloThere}>Hello, <span style="color: #063c81">{n}</span></h1>
+	<div class="list">
+		<input style="background: black; color: white;"bind:value={n}>
+		<br>
+		{#each todos as todo, index}
+		<div class="todo">
+			<input bind:value={todos[index]}>
+			<button on:click={()=>removeTodo(index)}>X</button>
+		</div>
+		<br>
+		{/each}
+		<button on:click={addTodo}>Add Something</button>
+	</div>
+</body>
